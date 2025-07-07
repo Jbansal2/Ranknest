aa68a8f634a045e7e55221f6d02207d1531321e2
Jitendra Bansal <146943450+Jbansal2@users.noreply.github.com>
2025-07-07 16:55:51 +0530

    Update auth.js

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -2,35 +2,24 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  createStudent,
   getAllStudents,
   getStudentsByBranch, 
-  getStudentByEnrollNo,
-  searchStudentsByName, 
-  updateStudentByEnrollNo,
   deleteStudentByEnrollNo,
-  addSemesterData, 
-  getStudentStats,
+
 } = require("../controllers/result");
 
 const { Query, getAllQueries } = require("../controllers/queries");
 const { sendEmail } = require("../controllers/email");
 
 // Student routes
-router.post("/students", createStudent);
-router.get("/students", getAllStudents);
-router.get("/students/stats", getStudentStats); 
-router.get("/students/search/name", searchStudentsByName); 
+
 router.get("/students/branch", getStudentsByBranch);
 router.get("/students/enroll_no", getStudentByEnrollNo);
-router.put("/students/:enroll_no", updateStudentByEnrollNo);
 router.delete("/students/:enroll_no", deleteStudentByEnrollNo);
-router.post("/students/:enroll_no/semesters", addSemesterData); 
+ 
 
 router.post("/Query", Query);
 router.get("/getAllQueries", getAllQueries);
-
-router.post("/email/send-result", sendEmail);
 
 
 module.exports = router;
